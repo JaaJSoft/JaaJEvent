db5dabdfa6ea0616c0b7dd7df1d4c455e8872959
pchopinet <pierre.chopinet97@gmail.com>
2020-05-27 14:55:18 +0200

    test

--- src/main/java/dev/jaaj/event/JaaJEventListener.java
+++ src/main/java/dev/jaaj/event/JaaJEventListener.java
@@ -5,6 +5,9 @@ import java.util.EventListener;
 @FunctionalInterface
 public interface JaaJEventListener<E extends JaaJEvent<?>> extends EventListener {
 
+    /**
+     * @param event event
+     */
     void handle(E event);
 
 }
